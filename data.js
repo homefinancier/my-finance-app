d5fc5d0620855817fc9f3b1085492e6a8b965ed5
homefinancier <123237007+homefinancier@users.noreply.github.com>
2025-10-04 21:59:36 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,23 @@
+// webapp/data.js
+
+const learningPath = [
+    {
+        id: "s01e01",
+        title: "Эпизод 1: Пробуждение",
+        url: "https://telegra.ph/Put-Andreya-EHpizod-1-Probuzhdenie-10-04",
+        type: "article" // Тип контента: статья
+    },
+    {
+        id: "s01e02",
+        title: "Эпизод 2: Иллюзия легких денег",
+        url: "https://telegra.ph/Put-Andreya-EHpizod-2-Illyuziya-legkih-deneg-10-04",
+        type: "article"
+    },
+    {
+        id: "calc01",
+        title: "Калькулятор 'Точка А'",
+        description: "Посчитайте свой текущий чистый капитал",
+        type: "tool" // Тип контента: инструмент
+    }
+    // Сюда мы будем добавлять новые шаги пути
+];
